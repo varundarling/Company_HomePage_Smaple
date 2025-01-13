eca3561d78f3bd9a2ab283a0fd1cea10b3f87724
Varun <varundarling7165@gmail.com>
2025-01-13 11:52:09 +0530

    Added Images

--- app/src/main/java/com/varun/sample/MainActivity.kt
+++ app/src/main/java/com/varun/sample/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.varun.sample
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,6 +14,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val logo : ImageView = findViewById(R.id.logo)
+        val bitmap = BitmapFactory.decodeResource(resources,R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources,bitmap)
+        rounded.cornerRadius = 50f
+        //rounded.isCircular = true
+        logo.setImageDrawable(rounded)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
