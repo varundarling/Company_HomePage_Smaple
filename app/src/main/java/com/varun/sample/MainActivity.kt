83907a4edeeebaf97da001981429386bcb8176bc
Varun <varundarling7165@gmail.com>
2025-01-13 13:26:42 +0530

    Added New Screen for Tablets

--- app/src/main/java/com/varun/sample/MainActivity.kt
+++ app/src/main/java/com/varun/sample/MainActivity.kt
@@ -21,12 +21,5 @@ class MainActivity : AppCompatActivity() {
         rounded.cornerRadius = 50f
         //rounded.isCircular = true
         logo.setImageDrawable(rounded)
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
